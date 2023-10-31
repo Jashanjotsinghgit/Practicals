a6eb483ab40b29e8429459ff78c6144b44a8433e
Jashanjotsinghgit <145551994+Jashanjotsinghgit@users.noreply.github.com>
2023-10-31 16:26:56 +0530

    Add files via upload

---
+++ absolutenumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,a;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    a=abs(n);
+    printf("Absolute number is: %d",a);
+    return 0;
+}
