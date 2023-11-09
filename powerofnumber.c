9d958c71628a9fd3f785ffa06a6ed11106b48b3a
Jashanjot Singh <145551994+Jashanjotsinghgit@users.noreply.github.com>
2023-11-09 08:01:05 +0530

    powerofnumber.c

--- powerofnumber.c
+++ powerofnumber.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,p,value=1;
+    int n,p;
     printf("Enter the Number: ");
     scanf("%d",&n);
     value=n;
     printf("Enter the power: ");
     scanf("%d",&p);
-    while(p>1)
-    {
-     value=value*n;
-     p--;
-    }
-    printf("Value is %d",value);
+    printf("Value is %d",power(n,p));
     return 0;
 }
+int power(int n,int p)
+{
+    int value=1;
+    while(p>=1)
+    {
+      value=value*n;
+       p--;
+    }
+    return value;
+}
+    
