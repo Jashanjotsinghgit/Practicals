8c5f6843da0486cf241fa90a185c9020ae7eb001
Jashanjot Singh <145551994+Jashanjotsinghgit@users.noreply.github.com>
2023-11-11 17:30:43 +0530

    structurenew.c

--- structurenew.c
+++ structurenew.c
@@ -44,3 +44,61 @@ int main()
     }
     return 0;
 }
+//QUESTION IN MSE-2
+#include<stdio.h>
+void display(int);
+void input(int);
+void highest(int);
+struct student{
+    char name[20];
+    int age;
+    float marks;
+    int Id;
+}p[100];
+int main()
+{
+    int n;
+    printf("Enter the number of Student: ");
+    scanf("%d",&n);
+    input(n);
+    display(n);
+    highest(n);
+    return 0;
+}
+void input( int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+    printf("Enter the %d Student name: ",i+1);
+    scanf("%s",&p[i].name);
+    printf("Enter I'D: ");
+    scanf("%d",&p[i].Id);
+    printf("Enter age: ");
+    scanf("%d",&p[i].age);
+    printf("Enter the Grade: ");
+    scanf("%f",&p[i].marks);
+    }
+}
+void display(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("\nName: %s",p[i].name);
+        printf("\nI'D : %d",p[i].Id);
+        printf("\nAge: %d",p[i].age);
+        printf("\nGrade: %5.2f",p[i].marks);
+    }
+}
+void highest(int n)
+{
+    int i,high=1;
+    for(i=0;i<n;i++)
+    {
+        if(p[i].marks > p[high].marks)
+        {
+          high=i;
+        }
+    }
+    printf("\nHighest Marks obtained by %s : %5.2f ",p[high].name,p[high].marks);
+}
